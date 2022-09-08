8442f143707c638999b9e7cf13c82ed583a057d2
kendra23 <kendrafrancis6@gmail.com>
2022-09-08 16:50:07 -0500

    Initial Commit

---
+++ starting new/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  starting new
+//
+//  Created by Kendra Francis on 9/8/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
